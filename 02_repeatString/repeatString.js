2a6e221cdfd6ad7603b57f2fb1fa4add5d86a579
stella2532 <stella25322532@gmail.com>
2022-02-28 19:16:25 -0700

    Complete repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    if (number < 0) {
+        return 'ERROR';
+    }
+    let result = '';
+    for (let i = 0; i < number; i++) {
+        result = result + string;
+    }
+    return result;
 };
 
 // Do not edit below this line
