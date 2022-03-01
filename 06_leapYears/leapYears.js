28104e1b011d6bd42ac7c8779a93c90d1860a30b
stella2532 <stella25322532@gmail.com>
2022-02-28 21:31:39 -0700

    Complete leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    switch (true) {
+        case year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0): 
+            return true;
+            break;
+        default:
+            return false;
+    }
 };
 
 // Do not edit below this line
