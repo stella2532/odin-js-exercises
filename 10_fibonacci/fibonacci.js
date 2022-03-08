3887fe1d37217e5f4ed8f0c10985219791a5843b
stella2532 <stella25322532@gmail.com>
2022-03-08 14:01:03 -0700

    Complete Fibonacci and add solutions

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,29 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+    if (x < 0) return 'OOPS';
+    if (Number(x) === 0) return '0';
 
+    let fib = [1, 1];
+    for (i = 1; i < Number(x); i++) {
+        let sum = fib[i] + fib[i-1];
+        fib.push(sum);
+    }
+    return fib[Number(x)-1];
 };
+
+/* answer key
+const fibonacci = function(count) {
+  if (count < 0) return "OOPS";
+  if (count === 0) return 0;
+  let a = 0;
+  let b = 1;
+  for (let i = 1; i < count; i++) {
+    const temp = b;
+    b = a + b;
+    a = temp;
+  }
+  return b;
+};
+*/
 
 // Do not edit below this line
 module.exports = fibonacci;
