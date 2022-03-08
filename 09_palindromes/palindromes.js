705f16e6de7244efb9a8f85e8d5f4f72fe937453
stella2532 <stella25322532@gmail.com>
2022-03-08 13:50:32 -0700

    Complete palindromes and include solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let palind = '';
+    for (i = str.length; i > 0; i--) {
+        palind += str.slice(i-1, i)
+    }
 
+    return str.toLowerCase().replace(/\W|_/g, '') === palind.toLowerCase().replace(/\W|_/g, '');
 };
+
+/* answer key
+const palindromes = function(string) {
+    processedString = string.toLowerCase().replace(/[^a-z]/g, "");
+    return (
+      processedString
+        .split("")
+        .reverse()
+        .join("") == processedString
+    );
+  };
+  */
 
 // Do not edit below this line
 module.exports = palindromes;
