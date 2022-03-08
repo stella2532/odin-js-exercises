7e534bd6ac98803ad8105de39b1e9b589cf2725a
stella2532 <stella25322532@gmail.com>
2022-03-08 13:12:18 -0700

    Complete calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(a,b) {
+	let sum = a + b;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  let minus = a - b;
+  return minus;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let finalSum = 0;
+  for (i = 0; i < arr.length; i++) {
+    finalSum += Number(arr[i]);
+  }
+  return finalSum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let mult = 1;
+  for (i = 0; i < arr.length; i++) {
+    mult *= Number(arr[i]);
+  }
+  return mult;
 };
 
-const power = function() {
-	
+const power = function(a, x) {
+	let result = 1;
+  for (i = x; i > 0; i--) {
+    result *= a;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let fact = 1;
+  for (i = a; i > 0; i--) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
